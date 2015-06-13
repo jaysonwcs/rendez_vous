6ae6cf37d92af135b975b9b66c4f4343980e5ce8
Jayson Weslley Cezar Silva <jaysonwcs@gmail.com>
2015-06-13 01:08:33 -0300

    first commit

---
+++ src/Main.hx
@@ -0,0 +1,21 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author Jayson Weslley Cezar Silva
+ */
+class Main extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+
+}
